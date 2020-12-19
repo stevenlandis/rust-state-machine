1966a1bf21967a9be63c83ae19e965abcb101c93
stevenlandis <stevenlandis@comcast.net>
2020-12-19 13:42:16 -0800

    finish nfa->dfa conversion tests
    not that there won't be more, but I tried most of the edge cases I
    wanted to reach.

--- src/fa.rs
+++ src/fa.rs
@@ -179,8 +179,8 @@ mod nfa {
 }
 
 mod dfa {
-  use super::nfa::{get_common_actions, get_parse_set, get_symbols, Nfa, ParseState};
-  use std::collections::{BTreeSet, HashMap};
+  use super::nfa;
+  use std::collections::{BTreeSet, HashMap, HashSet};
 
   struct Edge<A> {
     to: usize,
@@ -189,57 +189,85 @@ mod dfa {
   struct State<S, A> {
     edges: HashMap<S, Edge<A>>,
   }
-  struct Dfa<S, A> {
+  pub struct Dfa<S, A> {
     states: Vec<State<S, A>>,
     initial_state: usize,
   }
+  pub struct ParseState {
+    state: usize,
+  }
 
   impl<S: Eq + std::hash::Hash + Copy, A: Clone + PartialEq> Dfa<S, A> {
-    fn from_nfa(nfa: &Nfa<S, A>) -> Option<Dfa<S, A>> {
-      struct StateInfo<A> {
-        state: usize,
-        actions: ParseState<A>,
+    pub fn from_nfa(nfa: &nfa::Nfa<S, A>) -> Option<Dfa<S, A>> {
+      let initial_state = match nfa.get_initial_parse_state() {
+        None => {
+          return None;
+        }
+        Some(state) => state,
+      };
+      struct StackInfo {
+        state_idx: usize,
       }
-      let mut state_map: HashMap<BTreeSet<usize>, StateInfo<A>> = HashMap::new();
-      let mut states: Vec<State<S, A>> = Vec::new();
-      let mut stack: Vec<StateInfo<A>> = Vec::new();
+      struct StateInfo<S, A> {
+        dfa_state: State<S, A>,
+        nfa_state: nfa::ParseState<A>,
+      }
+
+      let mut state_map = HashMap::<BTreeSet<usize>, usize>::new();
+      let mut nfa_states = Vec::<nfa::ParseState<A>>::new();
+      let mut dfa_states = Vec::<State<S, A>>::new();
+      let mut stack = HashSet::<usize>::new();
+
+      state_map.insert(nfa::get_parse_set(&initial_state), 0);
+      nfa_states.push(initial_state);
+      dfa_states.push(State {
+        edges: HashMap::new(),
+      });
+      stack.insert(0);
+
       loop {
-        match stack.pop() {
+        println!("stack len={}", stack.len());
+        match stack.iter().next() {
           None => break,
-          Some(top) => {
-            if state_map.contains_key(&get_parse_set(&top.actions)) {
-              continue;
-            }
-            for symbol in get_symbols(nfa, &top.actions) {
-              match nfa.step(&top.actions, symbol) {
+          Some(state_idx) => {
+            let state_idx = *state_idx;
+            stack.remove(&state_idx);
+            println!(
+              "on state {}, symbols={}",
+              state_idx,
+              nfa::get_symbols(nfa, &nfa_states[state_idx]).len()
+            );
+            for symbol in nfa::get_symbols(nfa, &nfa_states[state_idx]) {
+              match nfa.step(&nfa_states[state_idx], symbol) {
                 None => return None,
                 Some(next_state) => {
-                  let (common_actions, next_state): (Vec<A>, ParseState<A>) =
-                    match get_common_actions(&next_state) {
+                  let (common_actions, next_state): (Vec<A>, nfa::ParseState<A>) =
+                    match nfa::get_common_actions(&next_state) {
                       None => (Vec::new(), next_state),
                       Some((common_actions, new_state)) => (common_actions, new_state),
                     };
-                  let key = get_parse_set(&next_state);
+                  let key = nfa::get_parse_set(&next_state);
                   let to = match state_map.get(&key) {
                     None => {
-                      let state_idx = states.len();
-                      state_map.insert(
-                        key,
-                        StateInfo {
-                          state: state_idx,
-                          actions: next_state,
-                        },
-                      );
-                      state_idx
+                      println!("edge to new state");
+                      let to_state_idx = dfa_states.len();
+                      nfa_states.push(next_state);
+                      dfa_states.push(State {
+                        edges: HashMap::new(),
+                      });
+                      state_map.insert(key, to_state_idx);
+                      stack.insert(to_state_idx);
+                      to_state_idx
                     }
-                    Some(info) => {
-                      if info.actions != next_state {
+                    Some(to_state_idx) => {
+                      println!("edge to existing state");
+                      if nfa_states[*to_state_idx] != next_state {
                         return None;
                       }
-                      info.state
+                      *to_state_idx
                     }
                   };
-                  states[top.state].edges.insert(
+                  dfa_states[state_idx].edges.insert(
                     symbol,
                     Edge {
                       to,
@@ -252,10 +280,24 @@ mod dfa {
           }
         }
       }
+      println!("result states={}", dfa_states.len());
       Some(Dfa {
-        states,
+        states: dfa_states,
         initial_state: 0,
       })
+    }
+
+    pub fn get_initial_parse_state(&self) -> ParseState {
+      ParseState {
+        state: self.initial_state,
+      }
+    }
+
+    pub fn step(&self, state: ParseState, symbol: S) -> Option<(ParseState, &Vec<A>)> {
+      match self.states[state.state].edges.get(&symbol) {
+        None => None,
+        Some(Edge { to, actions }) => Some((ParseState { state: *to }, actions)),
+      }
     }
   }
 }
@@ -354,5 +396,95 @@ mod nfa_tests {
 
     let state = nfa.step(&state, 1);
     assert_eq!(state, None);
+  }
+
+  #[test]
+  fn nfa_that_cannot_be_dfa_from_actions() {
+    let mut nfa = Nfa::<u32, u32>::new();
+    let s0 = nfa.add_state(true, false);
+    let s1 = nfa.add_state(true, false);
+    let s2 = nfa.add_state(false, false);
+    nfa.add_edge(s0, s0, 1, vec![0]);
+    nfa.add_edge(s0, s2, 2, vec![]);
+    nfa.add_edge(s1, s1, 1, vec![1]);
+    nfa.add_edge(s2, s2, 3, vec![]);
+
+    let state = nfa.get_initial_parse_state().unwrap();
+    assert_state(&state, &vec![(0, vec![]), (1, vec![])]);
+
+    let state = nfa.step(&state, 1).unwrap();
+    assert_state(&state, &vec![(0, vec![0]), (1, vec![1])]);
+
+    let state = nfa.step(&state, 1).unwrap();
+    let state = nfa.step(&state, 1).unwrap();
+    assert_state(&state, &vec![(0, vec![0, 0, 0]), (1, vec![1, 1, 1])]);
+
+    let state = nfa.step(&state, 2).unwrap();
+    assert_state(&state, &vec![(2, vec![0, 0, 0])]);
+  }
+}
+
+#[cfg(test)]
+mod dfa_tests {
+  use super::dfa;
+  use super::nfa;
+
+  #[test]
+  fn construct_simple_dfa_from_nfa() {
+    let mut nfa = nfa::Nfa::<u32, u32>::new();
+    let s0 = nfa.add_state(true, false);
+    let s1 = nfa.add_state(false, false);
+    let s2 = nfa.add_state(false, false);
+    nfa.add_edge(s0, s1, 1, vec![1]);
+    nfa.add_edge(s1, s2, 2, vec![2]);
+
+    let dfa = dfa::Dfa::from_nfa(&nfa).unwrap();
+    let state = dfa.get_initial_parse_state();
+
+    let (state, actions) = dfa.step(state, 1).unwrap();
+    assert_eq!(actions, &vec![1]);
+
+    let (state, actions) = dfa.step(state, 2).unwrap();
+    assert_eq!(actions, &vec![2]);
+
+    assert!(dfa.step(state, 3).is_none());
+  }
+
+  #[test]
+  fn construct_multiple_states() {
+    let mut nfa = nfa::Nfa::<u32, u32>::new();
+    let s0 = nfa.add_state(true, false);
+    let s1 = nfa.add_state(false, false);
+    let s2 = nfa.add_state(false, false);
+    let s3 = nfa.add_state(false, true);
+    nfa.add_edge(s0, s1, 1, vec![1]);
+    nfa.add_empty_edge(s0, s2, vec![1]);
+    nfa.add_edge(s1, s3, 2, vec![2]);
+    nfa.add_edge(s2, s3, 1, vec![]);
+
+    let dfa = dfa::Dfa::from_nfa(&nfa).unwrap();
+    let state = dfa.get_initial_parse_state();
+
+    let (state, actions) = dfa.step(state, 1).unwrap();
+    assert_eq!(actions, &vec![1]);
+
+    let (state, actions) = dfa.step(state, 2).unwrap();
+    assert_eq!(actions, &vec![2]);
+
+    assert!(dfa.step(state, 42).is_none());
+  }
+
+  #[test]
+  fn construct_fail_same_state_different_actions() {
+    let mut nfa = nfa::Nfa::<u32, u32>::new();
+    let s0 = nfa.add_state(true, false);
+    let s1 = nfa.add_state(true, false);
+    let s2 = nfa.add_state(false, false);
+    nfa.add_edge(s0, s0, 1, vec![0]);
+    nfa.add_edge(s0, s2, 2, vec![]);
+    nfa.add_edge(s1, s1, 1, vec![1]);
+    nfa.add_edge(s2, s2, 3, vec![]);
+
+    assert!(dfa::Dfa::from_nfa(&nfa).is_none());
   }
 }
